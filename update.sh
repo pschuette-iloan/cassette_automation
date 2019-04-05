c812a3ad659773201d59c947dafb97b36dc7cdd0
pschuette-iloan <pschuette@iloan.com>
2019-04-04 19:07:11 -0500

    session retrieved + session variables

--- update.sh
+++ update.sh
@@ -4,9 +4,26 @@
 # Script variables
 #
 output="$(pwd)/.output"
+temp="$(pwd)/.temp"
+cookies="$temp/cookies.txt"
+session_dir="$(pwd)/session"
 endpoints_dir="$(pwd)/endpoints"
+headers_cfg="$(pwd)/headers"
 baseurl="https://mobile.onemain.financial"
 
+# TODO: Check dependencies
+
+#
+# Create directory or remove contents
+#
+function clean_dir() {
+    echo "Cleaning Directory: $1"
+    if [ ! -d $1 ]
+    then mkdir $1
+    else
+        rm -rf "$1"/*
+    fi
+}
 
 #
 # Read a file into an array
@@ -28,18 +45,74 @@ function read_endpoint_configs() {
     endpoints=($(ls -d "$1"/*))
 }
 
+#
+# Read the standard headers from header file
+#
+function read_standard_headers() {
+    echo "reading standard headers from: $1"
+    source $1
+    # Print the headers
+    echo "Standard Headers:"
+    for header in "${headers[@]}"
+    do
+    echo $header
+    done
+}
 
 #
-# Setup the output directory
+# Setup the headers into a header file
 #
-function setup_output() {
-    echo "Ouput directory: $output"
-    if [ ! -d $output ]
-    then mkdir $output
-    else
-    echo removing contents of output directory
-    rm -rf "$output"/*
-    fi
+function setup_args() {
+    args="-H \"DISCO: $1\""
+    for header in "${headers[@]}"
+    do
+    args="$args -H \"$header\""
+    done
+    echo "Args: $args"
+}
+
+#
+# Call the session URL to create a new session
+#
+function prepare_session() {
+# $1 = scenario
+# $2 = endpoint config file
+# $3 = output directory
+
+    # source function variables
+    source $2
+
+    setup_args 'success'
+    cmd="curl -X $method $baseurl$endpoint_destination $args --cookie $cookies --cookie-jar $cookies -o $3/$output_file --verbose -u blah:blah"
+    echo "Calling: $cmd"
+    eval $cmd
+
+    # Setup the session args
+    token=$(cat $3/$output_file | jq -r '.data.attributes."access-token"')
+    challenge_type=$(cat $3/$output_file | jq -r '.data.attributes."device-challenge-type"')
+    challenge_id=$(cat $3/$output_file | jq -r '.data.relationships."device-challenges".data[0]."id"')
+    echo "Token: $token"
+    echo "Challenge Type: $challenge_type"
+    echo "Challenge ID: $challenge_id"
+
+    # May need to answer challenge questions
+}
+
+#
+# Call the endpoint and dump the output
+#
+function call_endpoint() {
+# $1 = scenario
+# $2 = endpoint config file
+# $3 = output directory
+    source $2
+
+    setup_args $1
+# Might need to switch between method types here
+    cmd="curl -i $args -X $method --data $data $baseurl$endpoint_destination -o $3/$output_file"
+    echo "Calling: $cmd"
+    eval $cmd
+
 }
 
 #
@@ -56,31 +129,45 @@ function main()
     # 2. get a list of all the mobile endpoints
     #
     read_endpoint_configs $endpoints_dir
-#    endpoints=(
-#            /api/v1/accounts
-#            /api/v1/configurations/global
-#        )
 
     #
-    # 3. Make the output directory
+    # 3. get the standard headers as variables
+    # Store a list of all standard headers in array
+    #
+    read_standard_headers $headers_cfg
+
+    #
+    # 4. Make the output directory
     # Hidden during creation
     #
-    setup_output
-
+    clean_dir $output
 
     for scenario in "${scenarios[@]}"
     do
         # Print out the scenario
         echo "Building scenario: $scenario"
+
+        clean_dir $temp
+
         # Make a directory for the scenario in the output
         scenario_dir="$(pwd)/.output/$scenario"
+
         mkdir $scenario_dir
+
+        prepare_session $scenario "$session_dir"/login $scenario_dir
+
         # Loop through the endpoints, pump into output directory
         for endpoint in "${endpoints[@]}"
         do
-        echo "Preparing endpoint: $endpoint"
+            echo "Would be Preparing endpoint: $endpoint"
+# call_endpoint $scenario $endpoint $scenario_dir
         done
+
+        # TODO: end session (delete)
     done
+
+    # Remove the temp directory
+    # rm -rf $temp
 }
 
 
